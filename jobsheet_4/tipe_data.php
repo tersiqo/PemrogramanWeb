54d3a652e3c18fce9a3fc23cf3ed0bb04da1777d
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-23 19:00:47 +0700

    Jobsheet 4

--- jobsheet_4/tipe_data.php
+++ jobsheet_4/tipe_data.php
@@ -33,4 +33,14 @@ var_dump($apakahSiswaLulus);
 echo "<br>";
 var_dump($apakahSiswaSudahUjian);
 
+$namaDepan = "Ibnu";
+$namaBelakang = 'Jakaria';
+
+$namaLengkap = "{$namaDepan} {$namaBelakang}";
+$namaLengkap2 = $namaDepan . ' ' . $namaBelakang;
+
+echo "Nama Depan : {$namaDepan} <br>";
+echo 'Nama Belakang: ' . $namaBelakang . '<br>';
+
+echo $namaLengkap;
 ?>
