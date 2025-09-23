57737c880ce1395912fdbf86caf319a19fa25bd3
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-23 18:52:09 +0700

    Jobsheet 4

--- jobsheet_4/tipe_data.php
+++ jobsheet_4/tipe_data.php
@@ -25,4 +25,12 @@ echo "Bahasa Indonesia: {$nilaiBahasaIndonesia} <br>";
 echo "Rata-rata: {$rataRata} <br>";
 
 var_dump($rataRata);
+
+$apakahSiswaLulus = true;
+$apakahSiswaSudahUjian = false;
+
+var_dump($apakahSiswaLulus);
+echo "<br>";
+var_dump($apakahSiswaSudahUjian);
+
 ?>
