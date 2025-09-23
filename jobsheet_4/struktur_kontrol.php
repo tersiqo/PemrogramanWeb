306d1cbee1d1c87adcc7527243dc07822754bc11
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-23 23:56:43 +0700

    Jobsheet 4

--- jobsheet_4/struktur_kontrol.php
+++ jobsheet_4/struktur_kontrol.php
@@ -22,4 +22,24 @@ while ($jarakSaatIni < $jarakTarget) {
 }
 echo "<br>";
 echo "Atlet tersebut memerlukan $hari hari untuk mencapai jarak 500 kilometer.";
+
+$jumlahLahan = 10;
+$tanamanPerLahan = 5;
+$buahPerTanaman = 10;
+$jumlahBuah = 0;
+
+for ($i = 1; $i <= $jumlahLahan; $i++) {
+    $jumlahBuah += ($tanamanPerLahan * $buahPerTanaman);
+}
+
+echo "Jumlah buah yang akan dipanen adalah: $jumlahBuah";
+
+$skorUjian = [85, 92, 78, 96, 88];
+$totalSkor = 0;
+
+foreach ($skorUjian as $skor) {
+    $totalSkor += $skor;
+}
+
+echo "Total skor ujian adalah: $totalSkor";
 ?>
