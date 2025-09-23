a0f914a8c057f140600ab19fe65883a228c6b075
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-24 00:56:24 +0700

    Jobsheet 4

---
+++ jobsheet_4/array.php
@@ -0,0 +1,34 @@
+<?php
+
+$nilaiSiswa = [85, 92, 78, 64, 90,55, 88, 79, 70, 96];
+
+$nilaiLulus = [];
+
+foreach ($nilaiSiswa as $nilai) {
+    if($nilai >= 70) {
+        $nilaiLulus[] = $nilai;
+    }
+}
+
+echo "Daftar nilai siswa yang lulus: " . implode(', ', $nilaiLulus);
+
+echo "<br>";
+$daftarKaryawan = [
+    ['Alice', 7],
+    ['Bob', 3],
+    ['Charile', 9],
+    ['David', 5],
+    ['Eva', 6],
+];
+
+$karyawanPengalamanLimaTahun = [];
+
+foreach ($daftarKaryawan as $karyawan) {
+    if ($karyawan[1] > 5) {
+        $karyawanPengalamanLimaTahun[] = $karyawan[0];
+    }
+}
+
+echo "Daftar karyawan dengan pengalaman kerja lebih dari 5 tahun: " . implode(',', 
+$karyawanPengalamanLimaTahun);
+?>
