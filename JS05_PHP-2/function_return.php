f074efd3667af37cdc2461cce071c8878c466c84
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-28 01:03:01 +0700

    jobsheet 5 php2

--- JS05_PHP-2/function_return.php
+++ JS05_PHP-2/function_return.php
@@ -3,6 +3,15 @@ function hitungUmur($thn_lahir, $thn_sekarang){
     $umur = $thn_sekarang - $thn_lahir;
     return $umur;
 }
+function perkenalan ($nama, $salam="Asssalamualaikum") {
+    echo $salam.",";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
 
-echo "Umur saya adalah ". hitungUmur(2006, 2023) ."tahun"
+    //memanggil fungsi lain
+    echo "Saya berusia ". hitungUmur(2006, 2023) ." tahun<br/>";
+    echo "Senang berkenalan dengan anda<br/>";
+}
+
+//memanggil fungsi perkenalan
+perkenalan("Iqo");
 ?> 
