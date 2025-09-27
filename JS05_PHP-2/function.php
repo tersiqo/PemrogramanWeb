927b074a15e380a7ca39b30ef95b7a4f72015d77
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-28 00:51:10 +0700

    jobsheet 5 php2

--- JS05_PHP-2/function.php
+++ JS05_PHP-2/function.php
@@ -1,6 +1,6 @@
 <?php
 
-function perkenalan($nama, $salam){
+function perkenalan($nama, $salam="Assalamualaikum"){
     echo $salam.", ";
     echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
@@ -13,5 +13,5 @@ echo "<hr>";
 $saya = "Elok";
 $ucapanSalam = "Selamat pagi";
 
-perkenalan($saya, $ucapanSalam);
+perkenalan($saya);
 ?>
