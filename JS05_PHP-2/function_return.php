23732096dff8be5a48cfccd4035d90fd0e628993
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-28 00:55:54 +0700

    jobsheet 5 php2

---
+++ JS05_PHP-2/function_return.php
@@ -0,0 +1,8 @@
+<?php
+function hitungUmur($thn_lahir, $thn_sekarang){
+    $umur = $thn_sekarang - $thn_lahir;
+    return $umur;
+}
+
+echo "Umur saya adalah ". hitungUmur(2006, 2023) ."tahun"
+?> 
