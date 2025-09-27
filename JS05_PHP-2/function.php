90f943d2a71080cffd349775512cfd28ac793709
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-28 00:39:59 +0700

    jobsheet 5 php2

---
+++ JS05_PHP-2/function.php
@@ -0,0 +1,10 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Tersiqo Alfarezel<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan();
+?>
