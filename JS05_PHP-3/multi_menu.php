3c867dfb31d277526a11b985c4e45fe14aef1d43
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-28 12:37:29 +0700

    jobsheet 5 php function

--- JS05_PHP-3/multi_menu.php
+++ JS05_PHP-3/multi_menu.php
@@ -33,11 +33,19 @@ $menu = [
     ]
 ];
 
-function tampilkanMenuBertingkat (array$menu) {
+function tampilkanMenuBertingkat (array $menu) {
     echo "<ul>";
-    foreach ($menu as $key => $item) {
-        echo "<li>{$item['nama']}</li>";
+    
+    foreach ($menu as $item) {
+        echo "<li>{$item['nama']}";
+
+        if (isset($item['subMenu'])) {
+            tampilkanMenuBertingkat($item['subMenu']);
+        }
+        
+        echo "</li>";
     }
+    
     echo "</ul>";
 }
 
