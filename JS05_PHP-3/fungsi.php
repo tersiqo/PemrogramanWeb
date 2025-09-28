cea4288d728dc19c4400afd21474ac7dacf4654c
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-28 12:14:54 +0700

    jobsheet 5 php function

---
+++ JS05_PHP-3/fungsi.php
@@ -0,0 +1,15 @@
+<?php
+
+function perkenalan(){
+    echo "Assalamualaikum, ";
+    echo "Perkenalkan, nama saya Iqo<br/>";
+    echo "Senang berkenalan dengan Anda<br/>";
+}
+
+perkenalan();
+
+echo "<br>";
+
+perkenalan();
+
+?>
