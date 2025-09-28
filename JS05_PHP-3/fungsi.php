3d9ef11a406ba70cb4e5fe074363b0643b1477af
tersiqo <tersiqo.alfarezel@gmail.com>
2025-09-28 12:18:14 +0700

    jobsheet 5 php function

--- JS05_PHP-3/fungsi.php
+++ JS05_PHP-3/fungsi.php
@@ -1,15 +1,17 @@
 <?php
 
-function perkenalan(){
-    echo "Assalamualaikum, ";
-    echo "Perkenalkan, nama saya Iqo<br/>";
+function perkenalan($nama, $salam){
+    echo$salam.", ";
+    echo "Perkenalkan, nama saya ".$nama."<br/>";
     echo "Senang berkenalan dengan Anda<br/>";
 }
 
-perkenalan();
+perkenalan("Hamdana","Hallo");
 
-echo "<br>";
+echo "<hr>";
 
-perkenalan();
+$saya ="Elok";
+$ucapanSalam = "Selamat pagi";
 
+perkenalan($saya, $ucapanSalam);
 ?>
