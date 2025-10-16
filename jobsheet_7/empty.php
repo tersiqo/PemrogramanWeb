42e04b267d9fbef0992b1dd1d8422782a60d28d3
tersiqo <tersiqo.alfarezel@gmail.com>
2025-10-16 18:35:03 +0700

    jobsheet 7

--- jobsheet_7/empty.php
+++ jobsheet_7/empty.php
@@ -1,7 +1,12 @@
 <?php
 $myArray = array(); // array kosong
 if (empty($myArray)) {
-    echo "Array tidak terdefinisi atau kosong.";
+    echo "Array tidak terdefinisi atau kosong." . "<br>";
 } else {
-    echo "Array terdefinisi dan tidak kosong.";
+    echo "Array terdefinisi dan tidak kosong." . "<br>";
+}
+if (empty($nonExistentVar)) {
+    echo "Variabel tidak terdefinisi atau kosong";
+} else {
+    echo "Variabel terdefinisi dan tidak kosong.";
 }
