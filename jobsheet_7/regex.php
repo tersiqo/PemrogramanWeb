43a668ac53a4d7d523b563dce066552b4553f43a
tersiqo <tersiqo.alfarezel@gmail.com>
2025-10-17 19:59:42 +0700

    jobsheet 7

--- jobsheet_7/regex.php
+++ jobsheet_7/regex.php
@@ -15,9 +15,17 @@ if (preg_match($pattern, $text, $matches)) {
     echo "Tidak ada yang cocok!" . "<br>";
 }
 
-$pattern = '/apple/';
+$pattern = '/apple/'; 
 $replacement = 'banana';
 $text = 'I like apple pie.';
 $new_text = preg_replace($pattern, $replacement, $text);
 echo $new_text; 
+
+$pattern = '/go*d/';
+$text = 'god is good.';
+if (preg_match($pattern, $text, $matches)) {
+    echo "Cocokkan: " . $matches[0];
+} else {
+    echo "Tidak ada yang cocok!";
+}
 ?>
