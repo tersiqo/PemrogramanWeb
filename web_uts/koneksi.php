af04fb26fe1dc258f1b190773e16a7c56d6c08c1
tersiqo <tersiqo.alfarezel@gmail.com>
2025-10-23 16:42:04 +0700

    web uts

--- web_uts/koneksi.php
+++ web_uts/koneksi.php
@@ -3,7 +3,7 @@ $host = "localhost";
 $port = "5433";
 $dbname = "rentalmobil";
 $user = "postgres";
-$password = "iqo14july"; // ganti sesuai password PostgreSQL kamu
+$password = "12345678"; // ganti sesuai password PostgreSQL kamu
 
 $conn = pg_connect("host=$host port=$port dbname=$dbname user=$user password=$password");
 
