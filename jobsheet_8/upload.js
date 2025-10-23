6442928864023569b364319d6807e71bc7007a9e
tersiqo <tersiqo.alfarezel@gmail.com>
2025-10-23 22:28:37 +0700

    jobsheet 8

--- jobsheet_8/upload.js
+++ jobsheet_8/upload.js
@@ -1,23 +1,31 @@
-// upload.js
-$(document).ready(function(){
-    $('#upload-form').submit(function(e){
-        e.preventDefault();
+$(document).ready(function() {
+    
+    $('#file').change(function() {
+        if (this.files.length > 0) {
+            $('#upload-button').prop('disabled', false).css('opacity', 1);
+        } else {
+            $('#upload-button').prop('disabled', true).css('opacity', 0.5);
+        }
+    });
 
-        var formData = new FormData(this);
+    $('#upload-form').submit(function(e) {
+        e.preventDefault();
 
-        $.ajax({
-            type: 'POST',
-            url: 'upload_ajax.php',
-            data: formData,
-            cache: false,
-            contentType: false,
-            processData: false,
-            success: function(response){
-                $('#status').html(response);
-            },
-            error: function(){
-                $('#status').html('Terjadi kesalahan saat mengunggah file.');
-            }
-        });
-    });
+        var formData = new FormData(this);
+
+        $.ajax({
+            type: 'POST',
+            url: 'upload_ajax.php',
+            data: formData,
+            cache: false,
+            contentType: false,
+            processData: false,
+            success: function(response) {
+                $('#status').html(response);
+            },
+            error: function() {
+                $('#status').html('Terjadi kesalahan saat mengunggah file.');
+            }
+        });
+    });
 });
